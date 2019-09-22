945813ccf5c8729f3055272f2933b5a1ada70814
github1 <john.collier4@gmail.com>
2019-09-22 07:19:55 -0400

    don't cache http resources

--- libraries/http.rb
+++ libraries/http.rb
@@ -1,15 +1,10 @@
 module RemoteResource
   class HttpRemoteResource
     def download(context)
-      f = ::Chef::Resource::File::RemoteFile.new(context.cached_file, context.run_context)
+      f = ::Chef::Resource::File::RemoteFile.new(context.path, context.run_context)
       f.source context.source
       f.run_action :create_if_missing
-      if f.updated_by_last_action? || !context.is_present?
-        context.copy_from_cache
-        true
-      else
-        false
-      end
+      f.updated_by_last_action?
     end
   end
 end
